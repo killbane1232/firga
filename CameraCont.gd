255fb44bf756ed12525e6ece7318e190a92f68d2
duckl1ng-git <71219454+duckl1ng-git@users.noreply.github.com>
2020-09-16 10:09:54 +0300

    хуй

--- CameraCont.gd
+++ CameraCont.gd
@@ -40,3 +40,97 @@ func _on_Button_gui_input(event):
 
 func _on_ItemList_item_selected(index):
 	pass # Replace with function body.
+
+
+func _on_Menu_pressed():
+	print('Menu is open')
+	
+	get_node("Control/Menu").visible = false
+	get_node("Control/LeftButton").visible = false
+	get_node("Control/RightButton").visible = false
+	
+	get_node("Control/ChooseRoom").visible = true
+	get_node("Control/ExitGame").visible = true
+	get_node("Control/ExitMenu").visible = true
+	get_node("Control/Options").visible = true
+	
+	pass # Replace with function body.
+
+
+func _on_Menu_gui_input(event):
+	if event is InputEventScreenTouch:
+		print('Menu is open')
+		
+		get_node("Control/Menu").visible = false
+		get_node("Control/LeftButton").visible = false
+		get_node("Control/RightButton").visible = false
+	
+		get_node("Control/ChooseRoom").visible = true
+		get_node("Control/ExitGame").visible = true
+		get_node("Control/ExitMenu").visible = true
+		get_node("Control/Options").visible = true
+	pass # Replace with function body.
+
+
+func _on_ExitMenu_pressed():
+	print('Menu is closed')
+	
+	get_node("Control/Menu").visible = true
+	get_node("Control/LeftButton").visible = true
+	get_node("Control/RightButton").visible = true
+	
+	get_node("Control/ChooseRoom").visible = false
+	get_node("Control/ExitGame").visible = false
+	get_node("Control/ExitMenu").visible = false
+	get_node("Control/Options").visible = false
+	pass # Replace with function body.
+
+
+func _on_ExitMenu_gui_input(event):
+	if event is InputEventScreenTouch:
+		print('Menu is closed')
+		
+		get_node("Control/Menu").visible = true
+		get_node("Control/LeftButton").visible = true
+		get_node("Control/RightButton").visible = true
+	
+		get_node("Control/ChooseRoom").visible = false
+		get_node("Control/ExitGame").visible = false
+		get_node("Control/ExitMenu").visible = false
+		get_node("Control/Options").visible = false
+	pass # Replace with function body.
+
+
+func _on_ExitGame_pressed():
+	print('Goodbye')
+	get_tree().quit()
+	pass # Replace with function body.
+
+
+func _on_ExitGame_gui_input(event):
+	if event is InputEventScreenTouch:
+		print('Goodbye')
+		get_tree().quit()
+	pass # Replace with function body.
+
+
+func _on_Options_pressed():
+	print('Options is open')
+	pass # Replace with function body.
+
+
+func _on_Options_gui_input(event):
+	if event is InputEventScreenTouch:
+		print('Options is open')
+	pass # Replace with function body.
+
+
+func _on_ChooseRoom_pressed():
+	print('Choosing room')
+	pass # Replace with function body.
+
+
+func _on_ChooseRoom_gui_input(event):
+	if event is InputEventScreenTouch:
+		print('Choosing room')
+	pass # Replace with function body.
